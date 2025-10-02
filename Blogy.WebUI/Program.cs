24bfd500868945e1ba6150b69c53d437c5114786
Sinan Kaya <skaya.sinan@outlook.com>
2025-10-02 22:35:33 +0300

    added initial migration,entities and generic repositories

--- Blogy.WebUI/Program.cs
+++ Blogy.WebUI/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blogy.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
