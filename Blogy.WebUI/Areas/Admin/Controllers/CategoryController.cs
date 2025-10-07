5ebe73da516826976bcdd6ffc2615fd28c37113b
Sinan Kaya <kayaasinan@gmail.com>
2025-10-07 23:01:55 +0300

    added update and delete operations

--- Blogy.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -26,5 +26,26 @@ namespace Blogy.WebUI.Areas.Admin.Controllers
             await _categoryService.CreateAsync(categoryDto);
             return RedirectToAction(nameof(Index));
         }
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return View(category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(updateCategory);
+            }
+
+            await _categoryService.UpdateAsync(updateCategory);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _categoryService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
