3757f3a22f3b8256d16a228518522229298a1e93
Sinan Kaya <kayaasinan@gmail.com>
2025-10-09 23:36:10 +0300

    added view components

---
+++ Blogy.WebUI/ViewComponents/Admin-Layout/_AdminLayoutHeadComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents.Admin_Layout
+{
+    public class _AdminLayoutHeadComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blogy.WebUI/ViewComponents/Admin-Layout/_AdminLayoutScriptComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents.Admin_Layout
+{
+    public class _AdminLayoutScriptComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
