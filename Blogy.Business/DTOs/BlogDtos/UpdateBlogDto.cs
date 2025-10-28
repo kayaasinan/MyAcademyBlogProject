b7f3ac90e78c2362edc46bdbf98cb5bde88d6254
Sinan Kaya <kayaasinan@gmail.com>
2025-10-28 20:11:41 +0300

    _

--- Blogy.Business/DTOs/BlogDtos/UpdateBlogDto.cs
+++ Blogy.Business/DTOs/BlogDtos/UpdateBlogDto.cs
@@ -1,9 +1,4 @@
 ﻿using Blogy.Business.DTOs.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blogy.Business.DTOs.BlogDtos
 {
--- Blogy.Business/Services/BlogServices/BlogService.cs
+++ Blogy.Business/Services/BlogServices/BlogService.cs
@@ -1,14 +1,7 @@
 ﻿using AutoMapper;
 using Blogy.Business.DTOs.BlogDtos;
 using Blogy.DataAccess.Repositories.BlogRepositories;
-using Blogy.DataAccess.Repositories.CategoryRepositories;
 using Blogy.Entity.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blogy.Business.Services.BlogServices
 {
