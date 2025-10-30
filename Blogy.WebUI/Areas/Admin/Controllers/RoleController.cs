d68bccc5346eb17e0796f240e77f20f3fa5c6e19
Sinan Kaya <kayaasinan@gmail.com>
2025-10-30 17:43:14 +0300

    added roles

---
+++ Blogy.WebUI/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,38 @@
+﻿using Blogy.Entity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class RoleController(RoleManager<AppRole> _roleManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(AppRole role)
+        {
+            var result = await _roleManager.CreateAsync(role);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
