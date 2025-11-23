087526f200f689bfa5e9306d5149b3bf64bd40d1
Sinan Kaya <kayaasinan@gmail.com>
2025-11-23 22:32:39 +0300

    added new configurations

--- Blogy.WebUI/Areas/Admin/Controllers/BlogController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using PagedList.Core;
+using System.Drawing.Printing;
 
 namespace Blogy.WebUI.Areas.Admin.Controllers
 {
